3e6cb6bf82f9d0e3c6ed38d76a8ed715911dfc9d
Pena <dev.muhammedov@gmail.com>
2024-07-08 21:31:57 +0500

    contacts module wrapped in typescript with typedi refactor tool

--- backend/src/app/Notifications/Services/Service.ts
+++ backend/src/app/Notifications/Services/Service.ts
@@ -1,29 +1,35 @@
-
-import type User from '~/@types/User/User'
-import type Contact from '~/@types/Contact/Contact'
-import type { NotificationType, NotificationKey } from '../../../@types/Notification/NotificationTypes'
+import type Contact from '~/@types/Contact/Contact';
+import type User from '~/@types/User/User';
+import type {
+  NotificationKey,
+  NotificationType,
+} from '../../../@types/Notification/NotificationTypes';
 
 export namespace NotificationsServices {
-    export abstract class Service {
-        private usersDbApi
+  export abstract class Service {
+    private usersDbApi;
 
-        private contactsDbApi
+    private contactsDbApi;
 
-        protected constructor() {
-            this.usersDbApi = require('../../../db/api/users.js');
-            this.contactsDbApi = require('../../../db/api/contacts.js');
-        }
-
-        checkIfNotificationEnabled(user: User, type: NotificationType, key: NotificationKey): boolean {
-            return true
-        }
-
-        protected async getUser(id: string): Promise<User> {
-            return this.usersDbApi.findBy({ id })
-        }
-
-        protected async getContact(id: string): Promise<Contact> {
-            return this.contactsDbApi.findBy({ id })
-        }
+    protected constructor() {
+      this.usersDbApi = require('../../../db/api/users.js');
+      this.contactsDbApi = require('../../../db/api/contacts');
     }
+
+    checkIfNotificationEnabled(
+      user: User,
+      type: NotificationType,
+      key: NotificationKey,
+    ): boolean {
+      return true;
+    }
+
+    protected async getUser(id: string): Promise<User> {
+      return this.usersDbApi.findBy({ id });
+    }
+
+    protected async getContact(id: string): Promise<Contact> {
+      return this.contactsDbApi.findBy({ id });
+    }
+  }
 }
--- backend/src/roninServices/appointments.js
+++ backend/src/roninServices/appointments.js
@@ -1,33 +1,32 @@
+const AppointmentsDBApi = require('../db/api/appointments');
+const AppointmentsService = require('../services/appointments');
+const db = require('../db/models');
 
-const AppointmentsDBApi = require('../db/api/Appointments');
-const AppointmentsService = require('../services/Appointments');
-const db = require("../db/models");
-
-const eventEmitter = require('../utils/eventEmitter')
+const eventEmitter = require('../utils/eventEmitter');
 
 module.exports = class RoninAppointmentsService extends AppointmentsService {
-    static async create(data, currentUser) {
-        const transaction = await db.sequelize.transaction();
-        try {
-            const appointment = await AppointmentsDBApi.create(data, {
-                currentUser,
-                transaction,
-            });
+  static async create(data, currentUser) {
+    const transaction = await db.sequelize.transaction();
+    try {
+      const appointment = await AppointmentsDBApi.create(data, {
+        currentUser,
+        transaction,
+      });
 
-            await transaction.commit();
+      await transaction.commit();
 
-            eventEmitter.emit('appointmentCreated', appointment);
+      eventEmitter.emit('appointmentCreated', appointment);
 
-            return appointment;
-        } catch (error) {
-            await transaction.rollback();
-            throw error;
-        }
+      return appointment;
+    } catch (error) {
+      await transaction.rollback();
+      throw error;
     }
+  }
 
-    static async update(data, id, currentUser) {
-        await super.update(data, id, currentUser);
+  static async update(data, id, currentUser) {
+    await super.update(data, id, currentUser);
 
-        return await AppointmentsDBApi.findBy({ id: id });
-    }
-}
+    return await AppointmentsDBApi.findBy({ id: id });
+  }
+};
