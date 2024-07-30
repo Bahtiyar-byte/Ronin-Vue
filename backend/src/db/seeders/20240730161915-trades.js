709be8645089ec86cf377005c97078e555059578
Pena <dev.muhammedov@gmail.com>
2024-07-30 22:14:29 +0500

    trades seeding created

---
+++ backend/src/db/seeders/20240730161915-trades.js
@@ -0,0 +1,103 @@
+'use strict';
+const db = require('../models');
+const Trades = db.trades;
+const TradesData = [
+  {
+    name: 'Aluminum/ Flashing',
+  },
+  {
+    name: 'Box Gutters',
+  },
+  {
+    name: 'Chimney Cap',
+  },
+  {
+    name: 'Downspouts',
+  },
+  {
+    name: 'EPDM',
+  },
+  {
+    name: 'Flat Roof',
+  },
+  {
+    name: 'Gutter Cleaning',
+  },
+  {
+    name: 'Gutter Guards',
+  },
+  {
+    name: 'Gutters',
+  },
+  {
+    name: 'Metal',
+  },
+  {
+    name: 'Pressure Washing',
+  },
+  {
+    name: 'Service Repairs',
+  },
+  {
+    name: 'Service: Call Back',
+  },
+  {
+    name: 'Shingles',
+  },
+  {
+    name: 'Siding',
+  },
+  {
+    name: 'Silicone',
+  },
+  {
+    name: 'Sky light',
+  },
+  {
+    name: 'Slate',
+  },
+  {
+    name: 'Soffit/ Fascia',
+  },
+  {
+    name: 'Tile',
+  },
+  {
+    name: 'TPO',
+  },
+  {
+    name: 'Vent install',
+  },
+  {
+    name: 'Ventilation',
+  },
+];
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    console.log(
+      'hererer________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________',
+    );
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+
+    await Trades.bulkCreate(TradesData);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
